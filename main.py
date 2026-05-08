c301ddaef2a70a47c553e9aa8412ed833d3e3733
Lyam Samuel Cardona <ly.samu19@gmail.com>
2026-05-08 18:38:25 +0000

    zsasd

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from datetime import datetime
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent
-
+from fastapi import FastAPI
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
-
-templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
-
-
-@app.get("/", response_class=HTMLResponse)
-async def inicio(request: Request):
-    return templates.TemplateResponse(
-        name="formulario.html",
-        request=request,
-        context={"ano_actual": datetime.now().year},
-    )
+@app.get("/")
+def read_root():
+    return {"message": "¡FastAPI en Vercel!"}
